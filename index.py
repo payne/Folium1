255fb332435f4845163aa626d0a1ecdfacfa11a0
Matt Payne <Payne@MattPayne.org>
2021-02-17 17:55:53 -0600

    Switch to Choropleth class.

--- index.py
+++ index.py
@@ -3,23 +3,23 @@ import pandas as pd
 import os
 
 states = os.path.join('data', 'us-states.json')
-unemployement_data = os.path.join('data', 'us_unemployment.csv')
-state_data = pd.read_csv(unemployement_data)
+recurse_data = os.path.join('data', 'recurse.csv')
+state_data = pd.read_csv(recurse_data)
 
 m = folium.Map(location=[48, -102], zoom_start=3)
 
-m.choropleth(
-    geo_data=states,
-    name='choropleth',
-    data=state_data,
-    columns=['State', 'Unemployment'],
-    key_on='feature.id',
-    fill_color='YlGn',
-    fill_opacity=0.7,
-    line_opacity=0.2,
-    legend_name='Unemployment Rate %'
-)
+folium.Choropleth(
+ geo_data=states,
+ name='choropleth',
+ data=state_data,
+ columns=['State','Recursers'],
+ nan_fill_color='grey',
+ nan_fill_opacity=0.4,
+ key_on='feature.id',
+ fill_color='Blues',
+ legend_name='# of Recursers',
+fill_opacity=0.7
+).add_to(m)
 
-folium.LayerControl().add_to(m)
 
-m.save('map2.html')
+m.save('index.html')
