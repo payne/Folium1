2c00d44658bbe9f92422a853526ccf5b31c76202
Matt Payne <Payne@MattPayne.org>
2021-02-26 09:58:25 -0600

    countries245 borders are working but there is no data to choropleth with.

--- index.py
+++ index.py
@@ -18,14 +18,7 @@ def makeChoropleth(m, geo_data, data):
     ).add_to(m)
     return choropleth 
 
-def main():
-    states = os.path.join('data', 'us-states.json')
-    countries245 = os.path.join('data', '245countries.json')
-    recurse_data = os.path.join('data', 'recurse.csv')
-    state_data = pd.read_csv(recurse_data)
-
-    m = folium.Map(location=[48, -102], zoom_start=3)
-
+def conusChropleth(m, states, state_data, folium):
     choropleth = makeChoropleth(m, states, state_data)
     folium.LayerControl().add_to(m)
 
@@ -34,9 +27,20 @@ def main():
         folium.features.GeoJsonTooltip(['name'], labels=True)
     )
 
+def main():
+    states = os.path.join('data', 'us-states.json')
+    countries245 = os.path.join('data', '245countries.json')
+    recurse_data = os.path.join('data', 'recurse.csv')
+    state_data = pd.read_csv(recurse_data)
+
+    m = folium.Map(location=[48, -102], zoom_start=3)
+
+    # TODO: Get both chropleth maps to work.
+    # conusChropleth(m, states, state_data, folium)
+
+    choropleth = makeChoropleth(m, countries245, None)
+
     # TODO: How to get the number of Recursers as the tool tip?
-
-
     m.save('index.html')
 
 if __name__ == "__main__":
