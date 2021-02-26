ffee9641803730fc8310babac92701d0238d392b
Matt Payne <Payne@MattPayne.org>
2021-02-26 09:51:31 -0600

    Refactor to makeChoropleth function.

--- index.py
+++ index.py
@@ -2,34 +2,43 @@ import folium
 import pandas as pd
 import os
 
-states = os.path.join('data', 'us-states.json')
-recurse_data = os.path.join('data', 'recurse.csv')
-state_data = pd.read_csv(recurse_data)
+def makeChoropleth(m, geo_data, data):
+    choropleth = folium.Choropleth(
+    geo_data=geo_data,
+    name='choropleth',
+    data=data,
+    columns=['State','Recursers'],
+    nan_fill_color='grey',
+    nan_fill_opacity=0.4,
+    key_on='feature.id',
+    fill_color='YlGn',
+    legend_name='# of Recursers',
+    highlight = True,
+    fill_opacity=0.5
+    ).add_to(m)
+    return choropleth 
 
-m = folium.Map(location=[48, -102], zoom_start=3)
+def main():
+    states = os.path.join('data', 'us-states.json')
+    countries245 = os.path.join('data', '245countries.json')
+    recurse_data = os.path.join('data', 'recurse.csv')
+    state_data = pd.read_csv(recurse_data)
 
-choropleth = folium.Choropleth(
- geo_data=states,
- name='choropleth',
- data=state_data,
- columns=['State','Recursers'],
- nan_fill_color='grey',
- nan_fill_opacity=0.4,
- key_on='feature.id',
- fill_color='YlGn',
- legend_name='# of Recursers',
- highlight = True,
- fill_opacity=0.5
-).add_to(m)
+    m = folium.Map(location=[48, -102], zoom_start=3)
 
-folium.LayerControl().add_to(m)
+    choropleth = makeChoropleth(m, states, state_data)
+    folium.LayerControl().add_to(m)
 
-# Display Region Label
-choropleth.geojson.add_child(
-    folium.features.GeoJsonTooltip(['name'], labels=True)
-)
+    # Display Region Label
+    choropleth.geojson.add_child(
+        folium.features.GeoJsonTooltip(['name'], labels=True)
+    )
 
-# TODO: How to get the number of Recursers as the tool tip?
+    # TODO: How to get the number of Recursers as the tool tip?
 
 
-m.save('index.html')
+    m.save('index.html')
+
+if __name__ == "__main__":
+    main()
+
